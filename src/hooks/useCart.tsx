7bfc183604bca51e8e80e75f070171a5c3cbc4bc
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-04-24 14:57:28 -0300

    first commit

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,14 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,28 +30,88 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  const prevCartRef = useRef<Product[]>();
+
+  useEffect(() => {
+    prevCartRef.current = cart;
+  }, []);
+
+  const cartPreviousValue = prevCartRef.current ?? cart;
+
+  useEffect(() => {
+    if (cartPreviousValue !== cart) {
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(cart));
+    }
+  }, [cartPreviousValue, cart]);
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const stock = await api
+        .get(`/stock/${productId}`)
+        .then((res) => Number(res.data.amount));
+
+      const productExist = cart.find((item) => item.id === productId);
+      const currentAmount = productExist ? productExist.amount : 0;
+
+      if (currentAmount + 1 > stock) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      if (productExist) {
+        setCart((state) => {
+          return state.map((item) => {
+            if (item.id === productExist.id) {
+              item.amount = item.amount + 1;
+            }
+
+            return item;
+          });
+        });
+      } else {
+        const product = (await api
+          .get(`products/${productId}`)
+          .then((res) => res.data)) as Product;
+
+        const cartCopy = [...cart];
+
+        cartCopy.push({
+          ...product,
+          amount: 1,
+        });
+
+        setCart(cartCopy);
+      }
     } catch {
       // TODO
+      toast.error("Erro na adição do produto");
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
       // TODO
+      const productExits = cart.find((item) => item.id === productId);
+      if (productExits) {
+        const cartFiltered = cart.filter((item) => item.id !== productId);
+
+        console.log(cartFiltered);
+        setCart(cartFiltered);
+      } else {
+        throw new Error();
+      }
     } catch {
       // TODO
+      toast.error("Erro na remoção do produto");
     }
   };
 
@@ -53,9 +120,38 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return;
+      }
+
+      const currentProduct = cart.find((item) => item.id === productId);
+
+      const stock = await api
+        .get(`/stock/${productId}`)
+        .then((res) => res.data.amount);
+
+      if (amount > stock) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      if (!currentProduct) {
+        throw Error();
+      }
+
+      setCart((state) => {
+        const updatedProduct = state.map((item) => {
+          if (item.id === productId) {
+            item.amount = amount;
+          }
+          return item;
+        });
+
+        return updatedProduct;
+      });
     } catch {
       // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
